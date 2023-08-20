4d47c58759eaa7ec511a52959671101aed84c0c6
sekar13 <110684623+sekar13@users.noreply.github.com>
2023-08-20 19:35:36 +0530

    Fifth commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express();
-//@route   GET api/users
-//@desc    Test route
+const { check , validationResult} = require('express-validator');
+//@route   Post api/users
+//@desc    Register user
 //@access  public
-router.get('/',(req,res) => res.send("User route"));
+router.post('/',[
+    check('name','Name is Required')
+        .not()
+        .isEmpty(),
+    check('email','Please include a valid email ').isEmail(),
+    check('password', 'please enter the password with 6  ot more characters').isLength({ min: 6 })
+
+],(req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+    res.send("User route");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const connectDB = require('./config/db')
 const app= express();
 //connect Database
 connectDB();
+
+//Init middleware
+app.use(express.json({extended:false}))
+
 //Define Routes
 app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
