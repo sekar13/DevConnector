02b4943bc6e90356480d6a4ed77b2f9df8c1b45e
sekar13 <110684623+sekar13@users.noreply.github.com>
2023-08-20 15:16:09 +0530

    Third Commit

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const connectDB = require('./config/db')
 const app= express();
 //connect Database
 connectDB();
+//Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
 app.get('/' ,(req,res) => res.send(`API Running`));
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
