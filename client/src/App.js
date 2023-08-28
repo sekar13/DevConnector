12b1acc727564c72465343069ee5a1ff93d908da
sekar13 <110684623+sekar13@users.noreply.github.com>
2023-08-28 14:39:57 +0530

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Fragment } from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './componenets/layout/Navbar';
 import Landing from './componenets/layout/Landing';
+import Login from './componenets/auth/Login';
+import Register from './componenets/auth/Register';
+
 import './App.css';
 
 const App = () => 
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-      
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="register" element={<Register/>} />
+          <Route path="login" element={<Login/>}/>
+         
+          
+          
+        </Routes>
+        
+        
+      </Fragment>
+    </Router>
 
 
 export default App;
--- client/src/componenets/layout/Landing.js
+++ client/src/componenets/layout/Landing.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Landing = () => {
   return (
@@ -8,8 +9,8 @@ const Landing = () => {
                 <h1 className="x-large">Developer Connector</h1>
                 <p className="lead">Create Developer profile/portfolio share  posts and get help from other developers</p>
                 <div className="buttons">
-                    <a href="register.html" className="btn btn-primary">Sign Up</a>
-                    <a href="login.html" className="btn btn">Login</a>
+                    <Link to="/register" className="btn btn-primary">Sign Up</Link>
+                    <Link to="/login" className="btn btn">Login</Link>
                 </div>
             </div>
         </div>
