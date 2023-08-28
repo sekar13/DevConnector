d703cc7d606b8cad0f684d8dd6fa48d9ba6900cc
sekar13 <110684623+sekar13@users.noreply.github.com>
2023-08-28 10:37:34 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Navbar from './componenets/layout/Navbar';
+import Landing from './componenets/layout/Landing';
+import './App.css';
+
+const App = () => 
+    <Fragment>
+      <Navbar/>
+      <Landing/>
+      
+    </Fragment>
+
+
+export default App;
